355034d51e159185663698c5075995f4c62f066c
tdudink <74917637+tdudink@users.noreply.github.com>
2026-04-26 13:09:09 +0200

    Added dbadash database grants for service

--- DBADash_SQLSecurity.sql
+++ DBADash_SQLSecurity.sql
@@ -3,7 +3,6 @@ DbaDash_SQLSecurity.sql
 
 Modification History:
 ---------------------
-2026-04-26  tdu  $(ComputerName)\svc_dbadash is now the default monitoring account for DbaDash.  Updated script to create login and grant permissions to this account.
 2026-04-25  tdu  use .\svc_dbadash as default monitoring account
 2024-12-21  tino/opt  reated script with initial content
 */
@@ -41,3 +40,17 @@ ALTER ROLE [SQLAgentReaderRole] ADD MEMBER ' + QUOTENAME(@sSQLLogin) + ';
 '
 PRINT @sSQLCMD
 EXEC sp_executesql @sSQLCMD
+ 
+SET @sSQLCMD = N'
+USE [dbadash]
+IF NOT EXISTS(SELECT [name]  
+			FROM [dbadash].sys.database_principals
+			WHERE name = ' + QUOTENAME(@sSQLLogin,'''') + ')
+BEGIN
+	CREATE USER ' + QUOTENAME(@sSQLLogin) + ' FOR LOGIN ' + QUOTENAME(@sSQLLogin) + ';
+END
+ALTER ROLE [db_owner] ADD MEMBER ' + QUOTENAME(@sSQLLogin) + ';
+';
+PRINT @sSQLCMD
+EXEC sp_executesql @sSQLCMD
+
